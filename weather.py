2650da329c82cedec54da08a05c7c5cb2ce9ac2e
Dias Yerbolov <Dias_Yerbolov@epam.com>
2021-12-31 14:39:36 +0600

    Add Weather Task

---
+++ weather.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+import os
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="Get the current weather information for your zipcode")
+parser.add_argument('zip', help="zip/postal code to get weather for")
+parser.add_argument('--country', default="us", help="Country zip/postal belongs to, default is 'us'")
+
+args = parser.parse_args()
+
+api_key = os.getenv("OWI_API_KEY")
+
+if not api_key:
+    print("Error: no 'OWM_API_KEY' provided")
+    sys.exit(1)
+url = f"http://api.openweathermap.org/data/2.5/weather?zip={args.zip},{args.country}&appid={api_key}"
+
+res = requests.get(url)
+print(res)
